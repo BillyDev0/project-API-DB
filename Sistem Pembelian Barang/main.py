132f91b43f93fdedfd1c1d49855a9d1dec0338c9
BillyDev0 <haloi10987@gmail.com>
2026-05-25 21:18:22 +0700

    menambahkan fitur beli barang di sistem pembelian barang

--- Sistem Pembelian Barang/main.py
+++ Sistem Pembelian Barang/main.py
@@ -17,3 +17,36 @@ class Barang(base):
     stok_barang=Column(Integer)
 
 base.metadata.create_all(engine)
+
+@app.get('/data_barang')
+def beli_barang(nama_barang:str,jumlah_beli:int):
+    if not nama_barang.strip():
+        return{'msg':'input nama_barang hanya berisi spasi'}
+    
+    nama_barang=nama_barang.strip().lower()
+
+    if jumlah_beli is not None:
+        if jumlah_beli<=0:
+           return{'msg':'Data tidak valid'}
+        get_nama=session.query(Barang).filter(Barang.nama_barang==nama_barang).first()
+        if not get_nama:
+            return{'msg':'Data tidak ditemukan'}
+        
+        if jumlah_beli>get_nama.stok_barang:
+            return{'msg':'stok barang tidak cukup'}
+
+    
+    cek_nama_barang=session.query(Barang).filter(Barang.nama_barang==nama_barang).first()
+    if not cek_nama_barang:
+        return{'msg':'Data tidak ditemukan'}
+    
+    cek_nama_barang.stok_barang-=jumlah_beli
+    total_bayar=jumlah_beli * cek_nama_barang.harga_barang
+    
+    session.commit()
+    return{'nama_barang':cek_nama_barang.nama_barang,
+           'jumlah_beli':jumlah_beli,
+           'total_bayar':total_bayar,
+           'sisa_stok':cek_nama_barang.stok_barang
+            }
+          
