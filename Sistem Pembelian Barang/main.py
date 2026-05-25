231a7b451b29decb8967e712ea8feea26b24e547
BillyDev0 <haloi10987@gmail.com>
2026-05-25 21:49:43 +0700

    membuat data lebih clean

--- Sistem Pembelian Barang/main.py
+++ Sistem Pembelian Barang/main.py
@@ -20,25 +20,23 @@ base.metadata.create_all(engine)
 
 @app.get('/data_barang')
 def beli_barang(nama_barang:str,jumlah_beli:int):
-    if not nama_barang.strip():
-        return{'msg':'input nama_barang hanya berisi spasi'}
     
     nama_barang=nama_barang.strip().lower()
 
-    if jumlah_beli is not None:
-        if jumlah_beli<=0:
-           return{'msg':'Data tidak valid'}
-        get_nama=session.query(Barang).filter(Barang.nama_barang==nama_barang).first()
-        if not get_nama:
-            return{'msg':'Data tidak ditemukan'}
-        
-        if jumlah_beli>get_nama.stok_barang:
-            return{'msg':'stok barang tidak cukup'}
+    if not nama_barang:
+        return{'msg':'input nama_barang hanya berisi spasi'}
 
-    
     cek_nama_barang=session.query(Barang).filter(Barang.nama_barang==nama_barang).first()
+
     if not cek_nama_barang:
         return{'msg':'Data tidak ditemukan'}
+
+    if jumlah_beli<=0:
+       return{'msg':'Data tidak valid'}
+    
+    if jumlah_beli>cek_nama_barang.stok_barang:
+        return{'msg':'stok barang tidak cukup'}
+
     
     cek_nama_barang.stok_barang-=jumlah_beli
     total_bayar=jumlah_beli * cek_nama_barang.harga_barang
@@ -49,4 +47,3 @@ def beli_barang(nama_barang:str,jumlah_beli:int):
            'total_bayar':total_bayar,
            'sisa_stok':cek_nama_barang.stok_barang
             }
-          
